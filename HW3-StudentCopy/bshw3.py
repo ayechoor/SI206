fc071910496112aa795e565cbc0474304535477d
Aparna Yechoor <ayechoor@umich.edu>
2016-11-08 22:39:52 -0500

    imported html

--- HW3-StudentCopy/bshw3.py
+++ HW3-StudentCopy/bshw3.py
@@ -1,5 +1,6 @@
 # Use https://www.si.umich.edu/programs/bachelor-science-
 # information/bsi-admissions as a template.
+ 
 # STEPS 
 # Create a similar HTML file but 
 # 1) Replace every occurrence of the word “student” with “AMAZING
@@ -10,3 +11,19 @@
 
 # Deliverables
 # Make sure the new page is uploaded to your GitHub account.
+
+import requests
+from bs4 import BeautifulSoup
+import re
+
+base_url = 'http://collemc.people.si.umich.edu/data/bshw3StarterFile.html'
+r = requests.get(base_url)						#requests Beautiful Soup output of html code of webpage
+soup = BeautifulSoup(r.text, "html.parser")		#assigns to variable soup
+
+text=soup('p')
+l=[]
+for x in text:
+	s=re.findall('.*student.*',str(x))
+	for a in s:
+		l.append(a)
+print (l)
